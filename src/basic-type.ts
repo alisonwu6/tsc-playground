1af386383262c6f24c73797d9d62aed886561cbd
Alison Wu <alison.wu23@gmail.com>
2023-04-28 15:49:25 +0800

    feat: ditto

---
+++ src/basic-type.ts
@@ -0,0 +1,24 @@
+console.log("Basic Types");
+
+// number
+let num1 = 999;
+let num2: number;
+num2 = 100;
+
+// string
+let str1 = 'bruce';
+let str2: string;
+str2 = 'bruce2';
+
+// boolean
+let bool = false;
+let boo2: boolean;
+boo2 = true;
+
+// undefined
+let un1 = undefined;
+let un2: undefined;
+
+// null
+let n1 = null;
+let n2: null;
