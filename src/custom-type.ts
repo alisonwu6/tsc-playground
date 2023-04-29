a1ae9ae07cb5f80c2cdb3440a9ecea614810eb08
Alison Wu <alison.wu23@gmail.com>
2023-04-29 21:58:48 +0800

    feat: custom type

---
+++ src/custom-type.ts
@@ -0,0 +1,15 @@
+// custom type
+let num1: number
+let str1: string | boolean | number
+let str2: string | boolean | number
+
+type LiveName = string | boolean | number
+
+let livename: LiveName
+let livename1: LiveName
+let livename2: LiveName
+
+// object
+type OBJ = { name: string, age: number }
+
+let obj: OBJ
